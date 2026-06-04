1e1d1548bdd89267a9dde8a4dcc49d3ae553855b
廖攀 <liaopan@MacBook-Pro-pl.local>
2026-06-04 11:14:30 +0800

    防浏览器缓存，SW改为网络优先

--- sw.js
+++ sw.js
@@ -1,12 +1,22 @@
-const CACHE = 'chatlink-v1';
-const FILES = ['/', '/index.html', '/manifest.json'];
+const CACHE = 'chatlink-v2';
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => {
+    return Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)));
+  }));
 });
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+    fetch(e.request).then(res => {
+      const clone = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, clone));
+      return res;
+    }).catch(() => caches.match(e.request))
   );
 });
